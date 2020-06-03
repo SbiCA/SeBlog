b1e972f102b72ae5dee2232a49d1eae2fb3e2ccd
simi <simon@artify.email>
2020-06-03 07:00:42 +0200

    Use logger instead of console writeline and set minimum level

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace SeBlog
 {
@@ -14,6 +15,8 @@ namespace SeBlog
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
+            builder.Logging.SetMinimumLevel(LogLevel.Information);
+            
             builder.Services.AddTransient(sp => new HttpClient
                 {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
